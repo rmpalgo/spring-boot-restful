a5f436feff0bef87f43df268bd3be0e0051cdb2c
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-08-04 22:31:55 -0500

    feat: complete crud web app with restful web service.

--- src/main/java/com/ronnelpalencia/springbootrestcrudpractice/controllers/ProductController.java
+++ src/main/java/com/ronnelpalencia/springbootrestcrudpractice/controllers/ProductController.java
@@ -28,12 +28,12 @@ public class ProductController {
         return productService.getProducts();
     }
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/productById/{id}")
     public Product findProductById(@PathVariable int id) {
         return productService.getProductById(id);
     }
 
-    @GetMapping("/products/{name}")
+    @GetMapping("/product/{name}")
     public Product findProductByName(@PathVariable String name) {
         return productService.getProductByName(name);
     }
